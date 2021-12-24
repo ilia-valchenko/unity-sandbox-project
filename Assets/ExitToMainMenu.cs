fcf84c1c5a74c94d9146f11a03458802b086ddc1
ivalchanka <i.valchenk@hotmail.com>
2021-12-24 23:58:22 +0300

    Added a simple unity3d project.

---
+++ Assets/ExitToMainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitToMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public int integerValue = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start writing logs on Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
